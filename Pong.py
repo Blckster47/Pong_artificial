8b5313c2fb10685f9db6ab57e647b8770ee8ee2f
Blckster <stoian_stefan_2006@yahoo.com>
2022-08-01 18:17:32 +0300

    Add paddle+movement

--- Pong.py
+++ Pong.py
@@ -6,14 +6,18 @@ WIDTH, HEIGHT =700, 500
 FPS=60
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 
+
 WHITE = (255,255,255)
 BLACK = (0,0,0)
+
+PADDLE_HEIGHT, PADDLE_WIDTH =100, 20
 
 pygame.display.set_caption("PingPong")
 
 
 class Paddle:
     COLOR = WHITE
+    VEL = 4
 
     def __init__(self,x,y,width,height):
         self.x = x
@@ -22,23 +26,55 @@ class Paddle:
         self.height = height
 
     def draw_paddle(self, win):
-        pygame.draw.rectangle()
+        pygame.draw.rect(win, self.COLOR, (self.x, self.y, self.width, self.height))
+
+    def move(self, up = True):
+        if up:
+            self.y -= self.VEL
+        else:
+            self.y += self.VEL
 
 
-def draw(win):
+def draw(win, paddles):
     win.fill(BLACK)
+
+    for paddler in paddles:
+        paddler.draw_paddle(win)
+
     pygame.display.update()
+
+def handle_paddle_movement(keys , left_paddle, right_paddle):
+    if keys[pygame.K_w] and left_paddle.y - left_paddle.VEL >=0:
+        left_paddle.move(up=True)
+    if keys[pygame.K_s] and left_paddle.y + left_paddle.VEL + left_paddle.height <= HEIGHT:
+        left_paddle.move(up=False)
+
+    if keys[pygame.K_UP] and right_paddle.y - right_paddle.VEL >=0:
+        right_paddle.move(up=True)
+    if keys[pygame.K_DOWN] and right_paddle.y + right_paddle.VEL + right_paddle.height <= HEIGHT:
+        right_paddle.move(up=False)
+
 
 def main():
     run = True
     clock = pygame.time.Clock()
+
+    left_paddle = Paddle(10, HEIGHT//2 - PADDLE_HEIGHT//2,PADDLE_WIDTH,PADDLE_HEIGHT)
+    right_paddle = Paddle(WIDTH-10-PADDLE_WIDTH, HEIGHT//2 - PADDLE_HEIGHT//2,PADDLE_WIDTH,PADDLE_HEIGHT)
+
     while run:
         clock.tick(FPS)
-        draw(WIN)
+        draw(WIN, [left_paddle, right_paddle])
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        keys = pygame.key.get_pressed()
+
+        handle_paddle_movement(keys, left_paddle, right_paddle)
+
     pygame.quit()
 
 if __name__ == '__main__':
